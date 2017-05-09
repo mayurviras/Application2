e4ab03220a9cfa4eab357a2b8a7df007546788ee
Mayur <mayur@Mayurs-Mac-mini.local>
2017-05-09 12:08:22 +0530

    add one method

--- app/src/main/java/com/example/mayur/application2/Main2Activity.java
+++ app/src/main/java/com/example/mayur/application2/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.example.mayur.application2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+    }
+
+    public void method(View view)
+    {
+        startActivity(new Intent(this,Main2Activity.class));
+    }
+    private void method1()
+    {
+      startActivity(new Intent(this,Main2Activity.class));
     }
 }
